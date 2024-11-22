b559a3afa4ce9cdac24c195c13256e17239d732f
alkumar4 <alishakumar099@gmail.com>
2024-11-22 14:22:25 -0500

    YAY IT WORKED ON SMALL CRAWL VERY QUICKLY YAY YAY YAY

--- src/cis5550/jobs/Indexer.java
+++ src/cis5550/jobs/Indexer.java
@@ -25,7 +25,8 @@ public class Indexer {
 	private static final String ALR_INDEXED = "pt-alrindexed";
 	private static final String URL_REF = TableColumns.URL.value();
 	private static final String PAGE_REF = TableColumns.PAGE.value();
-	private static final String PUNCTUATION = ".,:;!?\'\"()-=/+{}[]_#$&";
+	private static final String PUNCTUATION = ".,:;!?\'\"()-=/+{}[]_#$&\\";
+	private static final String SPACE = " ";
 	public static void run(FlameContext context, String[] arr) throws Exception {
 		RowToPair lambda1 = (Row myRow) -> {
 			if (myRow.columns().contains(URL_REF) && myRow.columns().contains(PAGE_REF)) {
@@ -64,84 +65,49 @@ public class Indexer {
 					tag = true;
 				} else if (f._2().charAt(i) == '>') {
 					tag = false;
-					removedTags += " ";
+					removedTags += SPACE;
 				} else if (!tag && !PUNCTUATION.contains(f._2().charAt(i) + "") && f._2().charAt(i) != '\n' && f._2().charAt(i) != '\r' && f._2().charAt(i) != '\t') {
 					removedTags += f._2().charAt(i);
 				} else if (!tag) {
-					removedTags += " ";
+					removedTags += SPACE;
 				}
 			}
 			removedTags = removedTags.toLowerCase();
-			String[] wordsList = removedTags.split(" ");
+			String[] wordsList = removedTags.split(SPACE);
 			HashSet<String> words = new HashSet<>();
 			HashMap<String, String> wordPositions = new HashMap<>();
 			int index = 0;
 			
 			for (String word : wordsList) {
 				index++;
-
-				// word = removePunctuation(word);
-				if (word == null || word.equals(" ") || word.equals("")) {
+				if (word == null || word.equals(SPACE) || word.equals("")) {
 					continue;
 				}
 
 				words.add(word);
 				if (wordPositions.containsKey(word)) {
-					wordPositions.put(word, wordPositions.get(word) + " " + index);
+					wordPositions.put(word, wordPositions.get(word) + SPACE + index);
 				} else {
 					wordPositions.put(word, index + "");
 				}
-
-				// word = word.toLowerCase();
-//				if (word.contains(" ")) {
-//					List<String> spaceSplit = Arrays.asList(word.split(" "));
-//					for (String wordX : spaceSplit) {
-//						if (!wordX.equals("") && !wordX.equals(" ")) {
-//							words.add(wordX);
-//							if (wordPositions.containsKey(wordX)) {
-//								wordPositions.put(wordX, wordPositions.get(wordX) + " " + index);
-//							} else {
-//								wordPositions.put(wordX, index + "");
-//							}
-//						}
-//					}
-//				} else {
-//					words.add(word);
-//					if (wordPositions.containsKey(word)) {
-//						wordPositions.put(word, wordPositions.get(word) + " " + index);
-//					} else {
-//						wordPositions.put(word, index + "");
-//					}
-//				}
 			}
+
 			System.out.println(f._1());
 			for (String w : words) {
 				KVSClient kvsClient = context.getKVS();
 				try {
 					String val = URLDecoder.decode(f._1(), StandardCharsets.UTF_8) + ":" + wordPositions.get(w);
-					kvsClient.appendToRow(INDEX_TABLE, w, URL_REF, val, ",");
+					if (!w.equals("") && !w.equals(" ") && !val.equals("")) {
+						kvsClient.appendToRow(INDEX_TABLE, w, URL_REF, val, ",");
+					}
 				} catch (Exception e) {
-					LOGGER.error("Error: issue with input: " + w);
+					LOGGER.error("Error: issue: " + w);
 				}
 
 			}
 			return null;
 		};
 		FlamePairRDD inverted = myPairs.flatMapToPair(lambda3);
-		myPairs.destroy();
+		// myPairs.destroy();
 	}
-	
-//	private static String removePunctuation(String s) {
-//		String ans = "";
-//
-//		for (char c : s.toCharArray()) {
-//			if (!PUNCTUATION.contains(c + "") && c != '\n' && c != '\r' && c != '\t') {
-//				ans += c + "";
-//			} else {
-//				ans += " ";
-//			}
-//		}
-//
-//		return ans;
-//	}
 }
