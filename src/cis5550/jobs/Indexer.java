a3a94dca212a0aaac4020322575ce0c1f879a9b1
alkumar4 <alishakumar099@gmail.com>
2024-11-25 18:54:45 -0500

    pls work

--- src/cis5550/jobs/Indexer.java
+++ src/cis5550/jobs/Indexer.java
@@ -31,21 +31,75 @@ public class Indexer {
 		RowToPair lambda1 = (Row myRow) -> {
 			if (myRow.columns().contains(URL_REF) && myRow.columns().contains(PAGE_REF)) {
 				KVSClient kvsClient = context.getKVS();
+				FlamePair f = new FlamePair(URLDecoder.decode(myRow.get(URL_REF), StandardCharsets.UTF_8), myRow.get(PAGE_REF));
 				try {
-					// using hashed url as key
-					if (kvsClient.existsRow(INDEXED_TABLE, myRow.key())) {
+					if (alreadyTraversed(context, URLDecoder.decode(f._1(), StandardCharsets.UTF_8))) {
+						System.out.println("alr done: " + f._1());
 						return null;
-					} else {
-						kvsClient.putRow(TO_INDEX, myRow);
-						if (myRow.get(URL_REF) != null && myRow.get(PAGE_REF) != null) {
-							return new FlamePair(URLDecoder.decode(myRow.get(URL_REF), StandardCharsets.UTF_8), myRow.get(PAGE_REF));
-						} else {
-							return null;
-						}
 					}
 				} catch (Exception e) {
-					LOGGER.error("KVS error: putting/accessing alr-indexed table");
+					LOGGER.error("issue with alr traversed");
 				}
+
+				String removedTags = "";
+
+				removedTags = f._2().replaceAll("<[^>]*>", " ");
+				removedTags = removedTags.toLowerCase().replaceAll("[^a-z0-9\\s]", " ");
+
+				String[] wordsList = removedTags.split(SPACE);
+
+				HashSet<String> words = new HashSet<>();
+				HashMap<String, String> wordPositions = new HashMap<>();
+				int index = 0;
+
+				for (String word : wordsList) {
+					if (word == null || word.equals(SPACE) || word.equals("")) {
+						continue;
+					}
+					index++;
+					word = word.replaceAll("\\s", "");
+					words.add(word);
+					if (wordPositions.containsKey(word)) {
+						wordPositions.put(word, wordPositions.get(word) + SPACE + index);
+					} else {
+						wordPositions.put(word, index + "");
+					}
+				}
+
+				String url = URLDecoder.decode(f._1(), StandardCharsets.UTF_8);
+				System.out.println(url);
+				HashMap<String, String> myRowValueMap = new HashMap<>();
+				for (String w : words) {
+					try {
+						String val = url + ":" + wordPositions.get(w);
+						if (w != null && !w.equals("") && !w.equals(" ") && !val.equals("")) {
+							if (w.length() <= 25 && w.length() > 0) {
+								myRowValueMap.put(w, val);
+								// kvsClient.appendToRow(INDEX_TABLE, w, URL_REF, val, ",");
+							}
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						LOGGER.error("Error:" + w);
+					}
+				}
+				if (myRowValueMap != null && myRowValueMap.size() > 0) {
+					// kvsClient.batchAppendToRow(INDEX_TABLE, URL_REF, myRowValueMap);
+					try {
+						kvsClient.batchAppendToRow(INDEX_TABLE, URL_REF, myRowValueMap);
+					} catch (Exception e) {
+						LOGGER.error("Error in appending: " + url);
+					}
+				}
+
+				try {
+					Row urlIndexed = new Row(Hasher.hash(url));
+					urlIndexed.put(URL_REF, url);
+					kvsClient.putRow(INDEXED_TABLE, urlIndexed);
+				} catch (Exception e) {
+					LOGGER.error("error logging indexed urls");
+				}
+
 				return null;
 
 			} else {
@@ -53,73 +107,73 @@ public class Indexer {
 			}
 		};
 		FlamePairRDD myPairs = context.pairFromTable(CRAWL_TABLE, lambda1);
-		KVSClient tempClient = context.getKVS();
-		tempClient.delete(INDEXED_TABLE);
+//		KVSClient tempClient = context.getKVS();
+//		tempClient.delete(INDEXED_TABLE);
 		
-		PairToPairIterable lambda3 = (FlamePair f) -> {
-			if (alreadyTraversed(context, URLDecoder.decode(f._1(), StandardCharsets.UTF_8))) {
-				System.out.println("alr done: " + f._1());
-				return null;
-			}
-			KVSClient kvsClient = context.getKVS();
-
-			String removedTags = "";
-
-			removedTags = f._2().replaceAll("<[^>]*>", " ");
-			removedTags = removedTags.toLowerCase().replaceAll("[^a-z0-9\\s]", " ");
-
-			String[] wordsList = removedTags.split(SPACE);
-
-			HashSet<String> words = new HashSet<>();
-			HashMap<String, String> wordPositions = new HashMap<>();
-			int index = 0;
-			
-			for (String word : wordsList) {
-				if (word == null || word.equals(SPACE) || word.equals("")) {
-					continue;
-				}
-				index++;
-				word = word.replaceAll("\\s", "");
-				words.add(word);
-				if (wordPositions.containsKey(word)) {
-					wordPositions.put(word, wordPositions.get(word) + SPACE + index);
-				} else {
-					wordPositions.put(word, index + "");
-				}
-			}
-
-			String url = URLDecoder.decode(f._1(), StandardCharsets.UTF_8);
-			System.out.println(url);
-			HashMap<String, String> myRowValueMap = new HashMap<>();
-			for (String w : words) {
-				try {
-					String val = url + ":" + wordPositions.get(w);
-					if (w != null && !w.equals("") && !w.equals(" ") && !val.equals("")) {
-						if (w.length() <= 25 && w.length() > 0) {
-							myRowValueMap.put(w, val);
-							// kvsClient.appendToRow(INDEX_TABLE, w, URL_REF, val, ",");
-						}
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-					LOGGER.error("Error:" + w);
-				}
-			}
-			if (myRowValueMap != null && myRowValueMap.size() > 0) {
-				// kvsClient.batchAppendToRow(INDEX_TABLE, URL_REF, myRowValueMap);
-				try {
-					kvsClient.batchAppendToRow(INDEX_TABLE, URL_REF, myRowValueMap);
-				} catch (Exception e) {
-					LOGGER.error("Error in appending: " + url);
-				}
-			}
-			Row urlIndexed = new Row(Hasher.hash(url));
-			urlIndexed.put(URL_REF, url);
-			kvsClient.putRow(INDEXED_TABLE, urlIndexed);
-			return null;
-		};
-		FlamePairRDD inverted = myPairs.flatMapToPair(lambda3);
-		myPairs.destroy();
+//		PairToPairIterable lambda3 = (FlamePair f) -> {
+//			if (alreadyTraversed(context, URLDecoder.decode(f._1(), StandardCharsets.UTF_8))) {
+//				System.out.println("alr done: " + f._1());
+//				return null;
+//			}
+//			KVSClient kvsClient = context.getKVS();
+//
+//			String removedTags = "";
+//
+//			removedTags = f._2().replaceAll("<[^>]*>", " ");
+//			removedTags = removedTags.toLowerCase().replaceAll("[^a-z0-9\\s]", " ");
+//
+//			String[] wordsList = removedTags.split(SPACE);
+//
+//			HashSet<String> words = new HashSet<>();
+//			HashMap<String, String> wordPositions = new HashMap<>();
+//			int index = 0;
+//
+//			for (String word : wordsList) {
+//				if (word == null || word.equals(SPACE) || word.equals("")) {
+//					continue;
+//				}
+//				index++;
+//				word = word.replaceAll("\\s", "");
+//				words.add(word);
+//				if (wordPositions.containsKey(word)) {
+//					wordPositions.put(word, wordPositions.get(word) + SPACE + index);
+//				} else {
+//					wordPositions.put(word, index + "");
+//				}
+//			}
+//
+//			String url = URLDecoder.decode(f._1(), StandardCharsets.UTF_8);
+//			System.out.println(url);
+//			HashMap<String, String> myRowValueMap = new HashMap<>();
+//			for (String w : words) {
+//				try {
+//					String val = url + ":" + wordPositions.get(w);
+//					if (w != null && !w.equals("") && !w.equals(" ") && !val.equals("")) {
+//						if (w.length() <= 25 && w.length() > 0) {
+//							myRowValueMap.put(w, val);
+//							// kvsClient.appendToRow(INDEX_TABLE, w, URL_REF, val, ",");
+//						}
+//					}
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//					LOGGER.error("Error:" + w);
+//				}
+//			}
+//			if (myRowValueMap != null && myRowValueMap.size() > 0) {
+//				// kvsClient.batchAppendToRow(INDEX_TABLE, URL_REF, myRowValueMap);
+//				try {
+//					kvsClient.batchAppendToRow(INDEX_TABLE, URL_REF, myRowValueMap);
+//				} catch (Exception e) {
+//					LOGGER.error("Error in appending: " + url);
+//				}
+//			}
+//			Row urlIndexed = new Row(Hasher.hash(url));
+//			urlIndexed.put(URL_REF, url);
+//			kvsClient.putRow(INDEXED_TABLE, urlIndexed);
+//			return null;
+//		};
+//		FlamePairRDD inverted = myPairs.flatMapToPair(lambda3);
+//		myPairs.destroy();
 	}
 
 	private static boolean alreadyTraversed(FlameContext aContext, String aUrl) throws Exception {
