98898486ef7ceaa4ee898f1efb6e39097f1f28fb
kirby1926 <bakj@seas.upenn.edu>
2024-12-05 23:51:05 -0500

    path

--- merge-tables.py
+++ merge-tables.py
@@ -247,18 +247,16 @@ def identical_file_resolver_crawl(table1_path, table2_path, file_path_in_1, file
     print(target_worker_folder)
     print(table_name)
     print(d)
-    print(f)
-
 
     # Copy the file which has the most recent timestamp
     if os.path.getmtime(file_path_in_1) > os.path.getmtime(file_path_in_2):
-        target_file_path = target_worker_folder + "/" + table_name + "/" + d + "/" + f
+        target_file_path = target_worker_folder + "/" + table_name + "/" + d + "/" + file_path_in_1
         target_file = os.path.join(target_file_path)
         if not os.path.exists(target_file):
             shutil.copy(file_path_in_1, target_file)
             print("Copied file " + f + " from " + os.path.basename(table1_path) + " to the merged table because it has the most recent timestamp")
     else:
-        target_file_path = target_worker_folder + "/" + table_name + "/" + d + "/" + f
+        target_file_path = target_worker_folder + "/" + table_name + "/" + d + "/" + file_path_in_2
         target_file = os.path.join(target_file_path)
         if not os.path.exists(target_file):
             shutil.copy(file_path_in_2, target_file)
