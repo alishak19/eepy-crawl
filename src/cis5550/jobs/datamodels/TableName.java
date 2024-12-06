86d993e0a29c1b519246a4334b4b84da31e0e905
Kunli Zhang <kunlizhang7@gmail.com>
2024-12-06 10:51:02 -0500

    New index table name

--- src/cis5550/jobs/datamodels/TableName.java
+++ src/cis5550/jobs/datamodels/TableName.java
@@ -2,7 +2,7 @@ package cis5550.jobs.datamodels;
 
 public enum TableName {
     CRAWL_TABLE("pt-crawl"),
-    INDEX_TABLE("pt-index"),
+    INDEX_TABLE("at-index"),
     PAGERANK_TABLE("pt-pagerank"),
     CACHE_TABLE("pt-cache"),
     ;
