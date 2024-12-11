b0ce2a193cac216d7ce35c9efc3dec3675271158
alkumar4 <alishakumar099@gmail.com>
2024-12-11 12:03:41 -0500

    aaa

--- src/cis5550/frontend/EepyCrawlSearch.java
+++ src/cis5550/frontend/EepyCrawlSearch.java
@@ -39,7 +39,7 @@ public class EepyCrawlSearch {
             myQuery = myQuery.toLowerCase().trim();
 
             List<SearchResult> myResults = getSearchResults(myQuery);
-            System.out.println(myResults);
+            System.out.println("results: " + myResults);
             return JSONBuilders.buildSearchResults(myResults);
         };
     }
